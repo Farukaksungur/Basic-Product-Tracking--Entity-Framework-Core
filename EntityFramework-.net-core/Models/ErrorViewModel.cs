e072b81c277df8c354be8123df73ba320babd2c1
Farukaksungur <farukaksungur.ceng@gmail.com>
2021-02-11 11:15:07 +0300

    Add project files.

---
+++ EntityFramework-.net-core/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EntityFramework_.net_core.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
